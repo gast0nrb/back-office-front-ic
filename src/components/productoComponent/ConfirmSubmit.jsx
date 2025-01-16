2b21aee7a7b4bd12cfef86b21e04d1e97a732498
gast0nrb <developer.tongas@gmail.com>
2025-01-16 17:58:05 -0300

    Cambios listos

--- src/components/productoComponent/ConfirmSubmit.jsx
+++ src/components/productoComponent/ConfirmSubmit.jsx
@@ -1,21 +1,7 @@
 const ConfirmSubmit = ({ originalValue, product, confirm, setConfirm, mayor, detalle, codigo}) => {
 
-  async function updateProducto() {
-
-  }
   async function changePrecio () {
-    let method = 'PUT'
-    const url = `http://localhost:8000/api/v0.5/webintercar/productos/${codigo}`
-    if (originalValue.ListaProductos.length <= 1) {
-      method = 'POST'
-    }
-    try{
-      console.log(mayor, detalle)
-    }catch(err){
-      console.log("err")
-    }finally {
-      console.log("setear valores")
-    }
+   console.log("Hello world") 
     /*try {
        const fetchApi = await fetch(`http://localhost:8000/api/v0.5/webintercar/productos/${codigo}`, {
         method : 'PUT', 
@@ -88,7 +74,7 @@ const ConfirmSubmit = ({ originalValue, product, confirm, setConfirm, mayor, det
               </button>
               <button
                 className="bg-ic-black rounded-sm hover:text-ic-white hover:bg-ic-orange"
-                onClick={updateProducto}
+                onClick={changePrecio}
               >
                 Confirmar
               </button>
